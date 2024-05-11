20caffe79e8eface7c33ea968eef0267df0b535b
Derek Walton <derekwalton19@gmail.com>
2024-05-11 13:04:19 +0100

    Q3

--- index.js
+++ index.js
@@ -2,3 +2,25 @@
 /**
  * Add your functions here...
  */
+
+function targetTextToConsole(event){
+    console.log(event.target.textContent);
+}
+
+function tttcAttacher(){
+    const myButton = document.querySelector('#button0');
+    myButton.addEventListener('click', targetTextToConsole);
+}
+
+function lovelyParaAttacher(){
+    const myElem = document.querySelector('#thisisalovelyparagraph')
+    myElem.addEventListener('click', lovelyToggle)
+}
+
+function lovelyToggle(event){
+    if (event.target.classList.contains('lovely')){
+        event.target.classList.remove('lovely');
+    } else{
+        event.target.classList.add('lovely')
+    }
+}
