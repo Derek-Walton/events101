5909a09b59f089265e2ee1ef4391e1f9e5bb74dc
Derek Walton <derekwalton19@gmail.com>
2024-09-30 12:00:53 +0100

    30/9/24 redone

--- index.js
+++ index.js
@@ -4,76 +4,77 @@
  */
 
 function targetTextToConsole(event){
-    console.log(event.target.textContent);
+  console.log(event.target.textContent);
 }
 
 function tttcAttacher(){
-    const myButton = document.querySelector('#button0');
-    myButton.addEventListener('click', targetTextToConsole);
+  const button = document.querySelector('#button0');
+  button.addEventListener('click', targetTextToConsole);
 }
 
 function lovelyParaAttacher(){
-    const myElem = document.querySelector('#thisisalovelyparagraph')
-    myElem.addEventListener('click', lovelyToggle)
-}
-
-function lovelyToggle(event){
-    if (event.target.classList.contains('lovely')){
-        event.target.classList.remove('lovely');
-        event.target.classList.remove('lovely');
-    } else{
-        event.target.className = 'lovely';
-    }
+  const element = document.querySelector('#thisisalovelyparagraph');
+  element.addEventListener('click', lovelyToggle);
 }
 
 function lovelyButtonAttacher(){
-    const elem = document.querySelector('#button1');
-    elem.addEventListener('click', lovelyToggle);
+  const button = document.querySelector('#button1');
+  button.addEventListener('click', lovelyToggle);
 }
 
-function joinOut1(event){
-    console.log(event.target);
+function lovelyToggle(event){
+  if (event.target.classList.contains('lovely')){
+      event.target.classList.remove('lovely');
+  } else{
+      event.target.className = 'lovely';
+  }
 }
 
 function concatAttacher(){
-    const elem1 = document.querySelector('#in1');
-    const elem2 = document.querySelector('#in2');
-    const out1 = document.querySelector('#out1');
-    elem1.addEventListener('change', () => {out1.textContent = elem1.value + elem2.value})
-    elem2.addEventListener('change', () => {out1.textContent = elem1.value + elem2.value})
-}
+  const input1 = document.querySelector('#in1');
+  const input2 = document.querySelector('#in2');
 
-function snitchUpdater(event) { // eslint-disable-line no-unused-vars
-    window.snitch.textContent = (event.type === 'mouseover' ? 'IN' : 'OUT');
+  const output = document.querySelector('#out1');
+  input1.addEventListener('change', event => {
+    output.textContent = input1.value + input2.value;
+  });
+  input2.addEventListener('change', event => {
+    output.textContent = input1.value + input2.value;
+  });
 }
 
 function snitchAttacher(){
-    const elem = document.querySelector('#mousewatcher');
-    elem.addEventListener('mouseover', snitchUpdater);
-    elem.addEventListener('mouseout', snitchUpdater);
+  const div = document.querySelector('#mousewatcher');
+  const snitch = document.querySelector('#snitch');
+
+  div.addEventListener('mouseover', event => {
+    snitch.textContent = 'IN';
+  })
+  div.addEventListener('mouseout', event => {
+    snitch.textContent = 'OUT';
+  })
 }
 
-function reportAttacher(){
-    const mouseReporter = document.querySelector('#mousereporter');
-    mouseReporter.addEventListener('mousemove', reportUpdater);
+function reportAttacher() {
+  const mouseReporter = document.querySelector('#mousereporter');
+  
+  mouseReporter.addEventListener('mousemove', reportUpdater)
 }
 
-function reportUpdater(event){
-    event.target.firstChild.textContent = `x: ${event.screenX} y: ${event.screenY}`;
+function reportUpdater(event) {
+  const report = document.querySelector('#report');
+  report.textContent = 'x: ' + event.screenX + ' y: ' + event.screenY;
 }
 
 function idValidationAttacher(){
-    const newIdInput = document.querySelector('#newid');
-    newIdInput.addEventListener('input', idValidation);
-    // console.log(newIdInput.value)
+  const input = document.querySelector('#newid');
+  input.addEventListener('input', idValidation)
 }
 
 function idValidation(event){
-    if (event.target.value.includes(' ')){
-        event.target.classList.add('invalid');
-    } else {
-        if (event.target.classList.contains('invalid')){
-            event.target.classList.remove('invalid');
-        }
-    }
+  if (event.target.value.includes(' ')) {
+    event.target.classList.add('invalid');
+  } else {
+    event.target.classList.remove('invalid');
+  }
 }
